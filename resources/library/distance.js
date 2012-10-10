386b5933abc75fbdd7b1efd1205d6c10ce97d122
Moin <moinnadeem@moinnadeem.com>
2012-10-10 10:29:23 -0400

    changed function to use variable for javascript scope errors.

--- resources/library/distance.js
+++ resources/library/distance.js
@@ -3,7 +3,7 @@ function distance(x1, y1, x2, y2) {
     // Calculate X and Y values.
     var x = (x2 - x1) * (x2 - x1);
     var y = (y2 - y1) * (y2 - y1);
-    var distance = x+y;
+    var add = x+y;
     // Set, round, and display the distance. 100 is because it's rounded to 10^2 digit.
-    return (radsimplify(Math.round(distance * 100) / 100));
+    return (radsimplify(Math.round(add * 100) / 100));
 }
