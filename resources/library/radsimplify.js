9c266ac4be0b8becb70ae324461dfc300847795c
Moin Nadeem <moinnadeem@moinnadeem.com>
2012-10-09 23:34:58 -0500

    created radical simplifier, need to add comments. inital commit.

---
+++ resources/library/radsimplify.js
@@ -0,0 +1,13 @@
+function radsimplify(number) {
+	var outside = 1
+	var d = 2
+	while (d * d <= number) {
+		if (number % (d * d) == 0) {
+			number = number / (d * d);
+			outside = outside * d
+		} else {
+			d = d + 1;
+		}
+	}
+	return (number + "\u221A" + outside);
+}
