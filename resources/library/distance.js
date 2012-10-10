8d779caa0dd7baf4e6c493ec041310d43f07b945
Moin Nadeem <moinnadeem@moinnadeem.com>
2012-10-09 23:33:41 -0500

    automatically simplified square roots, added comment to include radsimplify.js

--- resources/library/distance.js
+++ resources/library/distance.js
@@ -1,8 +1,9 @@
 function distance(x1, y1, x2, y2) {
+	// REQUIRES: RADSIMPLIFY.JS
     // Calculate X and Y values.
     var x = (x2 - x1) * (x2 - x1);
     var y = (y2 - y1) * (y2 - y1);
-    var distance = Math.sqrt(x+y);
+    var distance = x+y;
     // Set, round, and display the distance. 100 is because it's rounded to 10^2 digit.
-    return Math.round(distance * 100) / 100;
+    return (radsimplify(Math.round(distance * 100) / 100));
 }
